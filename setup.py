a4227ea49536f2be562773482f190131666803b0
isna <isna11583@gmail.com>
2019-12-03 01:00:32 +0700

    Test update version

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name='filetype_validator',
   packages=['filetype_validator'],
-  version='0.1',
+  version='0.2',
   license='MIT',
   description=description_text,
   author='isna',
   author_email='isna11583@gmail.com',
   url='https://github.com/ixna/filetype_validator',
-  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.1',
+  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.2',
   keywords=['filetype', 'extension'],
   dependency_links=['https://github.com/ixna/filetype.py/tarball/master'],
   classifiers=[
