9b156d112650adab01c202ce6e95d0e9d4b5515c
isna <isna11583@gmail.com>
2019-12-03 11:56:07 +0700

    Fix dependency url

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name='filetype_validator',
   packages=['filetype_validator'],
-  version='0.2',
+  version='0.2.1',
   license='MIT',
   description=description_text,
   author='isna',
   author_email='isna11583@gmail.com',
   url='https://github.com/ixna/filetype_validator',
-  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.2',
+  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.2.1',
   keywords=['filetype', 'extension'],
-  dependency_links=['https://github.com/ixna/filetype.py/tarball/master'],
+  dependency_links=['git+https://github.com/ixna/filetype.py'],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
