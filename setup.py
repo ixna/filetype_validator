fb935ff42025fc87cc51533c27dbff5a8623a40a
isna <isna11583@gmail.com>
2019-12-03 14:10:34 +0700

    increment version

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name='filetype_validator',
   packages=['filetype_validator'],
-  version='0.2.5',
+  version='0.2.6',
   license='MIT',
   description=description_text,
   author='isna',
   author_email='isna11583@gmail.com',
   url='https://github.com/ixna/filetype_validator',
-  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.2.5',
+  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.2.6',
   keywords=['filetype', 'extension'],
   classifiers=[
     'Development Status :: 3 - Alpha',
